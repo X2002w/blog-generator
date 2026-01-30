11a59a20c7dd023d55c9add45f333da637a31333
X2002w <15008695828@163.com>
2026-01-31 02:26:15 +0800

    safer types

--- hello.hs
+++ hello.hs
@@ -1,9 +1,12 @@
-main = putStrLn myhtml
-myhtml = makeHtml 
-  "my title" 
-  (h1_ "hello, world!" <> p_ "this is webpage")
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+newtype Html = Html String
+newtype Structure = Structure String
+
+getStructureString :: Structure -> String
+getStructureString struct = 
+  case struct of
+    Structure str -> str
+
+-- main = putStrLn myhtml
 
 html_ :: String -> String
 html_ = el "html"
@@ -20,10 +23,34 @@ title_ = el "title"
 h1_ :: String -> String
 h1_ = el "h1"
 
-p_ :: String -> String
-p_ = el "p"
+-- (.) :: (b -> c) -> (a -> b) -> a -> c
+-- (.) :: (String -> Structure) -> (String -> String) -> (String -> Structure)
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
 el :: String -> String -> String
 el tag content = 
   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
+myhtml = makeHtml 
+  "my title" 
+  (h1_ "hello, world!" <> p_ "this is webpage")
+makeHtml :: String -> String -> String
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+
+-- selfincr function
+incr :: Int -> Int
+incr x = x + 1
+-- times2 function
+times2 :: Int -> Int
+times2 x = x * 2
+-- selfincr and times2
+addThenDouble :: Int -> Int
+addThenDouble = times2 . incr
+
+append_ :: Structure -> Structure -> Structure
+append_ s1 s2 = 
+  Structure (getStructureString s1 <> getStructureString s2)
+
+main = print (addThenDouble 3)
