c0cb4181dea39df9cebfdc4d1ea508e299d845d0
X2002w <15008695828@163.com>
2026-02-03 22:37:36 +0800

    add module

---
+++ Html.hs
@@ -0,0 +1,59 @@
+module Html
+  ( Html
+  , Title
+  , Structure
+  , html_
+  , p_
+  , h1_
+  , append_
+  , renderHtml
+  )
+  where
+ 
+newtype Html = Html String
+newtype Structure = Structure String
+type Title = String
+-- html lable -> head lable | body lable
+-- head lable -> title lable
+-- 先生成body(Structure), 再结合head(title)
+html_ :: Title -> Structure -> Html
+html_  title content = 
+  Html 
+    (
+      el "html"
+        (
+          el "head" (el "title" title) <>
+          el "body" (getStructureString content)
+        )
+    )
+
+getStructureString :: Structure -> String
+getStructureString content = 
+  case content of
+    Structure str -> str
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
+-- h1_ = \content -> Structure (el "h1" content)
+
+p_ :: String -> Structure
+p_ = Structure . el "p"
+-- p_ = \content -> Structure (el "p" content)
+
+el :: String -> String -> String
+el tag content = 
+  "<" <> tag <> ">\n" <> content <> "</" <> tag <> ">\n"
+
+append_ :: Structure -> Structure -> Structure
+append_ s1 s2 = 
+  Structure (getStructureString s1 <> getStructureString s2)
+-- 另一种写法
+-- append_ (Structure s1) (Structure s2) = 
+--   Structure (s1 <> s2)
+
+renderHtml :: Html -> String
+-- renderHtml (Html str) = str
+renderHtml content =
+  case content of
+    Html str -> str
+ 
--- hello.hs
+++ hello.hs
@@ -1,5 +1,6 @@
-main :: IO ()
+import Html
 
+main :: IO ()
 main = putStrLn (renderHtml myhtml)
 
 myhtml :: Html
@@ -16,50 +17,3 @@ myhtml =
         )
     )
 
-
-newtype Html = Html String
-newtype Structure = Structure String
-type Title = String
--- html lable -> head lable | body lable
--- head lable -> title lable
--- 先生成body(Structure), 再结合head(title)
-html_ :: Title -> Structure -> Html
-html_  title content = 
-  Html 
-    (
-      el "html"
-        (
-          el "head" (el "title" title) <>
-          el "body" (getStructureString content)
-        )
-    )
-
-getStructureString :: Structure -> String
-getStructureString content = 
-  case content of
-    Structure str -> str
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
--- h1_ = \content -> Structure (el "h1" content)
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
--- p_ = \content -> Structure (el "p" content)
-
-el :: String -> String -> String
-el tag content = 
-  "<" <> tag <> ">\n" <> content <> "</" <> tag <> ">\n"
-
-append_ :: Structure -> Structure -> Structure
-append_ s1 s2 = 
-  Structure (getStructureString s1 <> getStructureString s2)
--- 另一种写法
--- append_ (Structure s1) (Structure s2) = 
---   Structure (s1 <> s2)
-
-renderHtml :: Html -> String
--- renderHtml (Html str) = str
-renderHtml content =
-  case content of
-    Html str -> str
